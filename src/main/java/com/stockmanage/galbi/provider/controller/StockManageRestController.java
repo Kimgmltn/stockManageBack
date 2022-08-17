7751f2669291393d07779b3aa88c0298a80a1529
Kimgmltn <Kimgmltn0323@gmail.com>
2022-08-18 00:04:05 +0900

    로컬환경 변경

--- src/main/java/com/stockmanage/galbi/provider/controller/StockManageRestController.java
+++ src/main/java/com/stockmanage/galbi/provider/controller/StockManageRestController.java
@@ -1,5 +1,6 @@
 package com.stockmanage.galbi.provider.controller;
 
+import com.stockmanage.galbi.entity.Stock;
 import com.stockmanage.galbi.provider.request.ProviderSaveRequest;
 import com.stockmanage.galbi.provider.service.ProviderService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +25,7 @@ public class StockManageRestController {
 
     @GetMapping("/stocks")
     @CrossOrigin(origins = {"*"}, allowedHeaders = {"*"})
-    public ResponseEntity findCodes(){
+    public ResponseEntity<List<Stock>> findCodes(){
         return ResponseEntity.ok(providerService.findAll());
     }
 
